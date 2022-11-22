1358d79c659869b9488c07fed3d681add837dfe6
guisantosalves <workguisantos@gmail.com>
2022-11-22 09:56:21 -0400

    styling input component and improving it

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
 
 const ChatInput = () => {
-    return(
-        <div>
-            <h1>chat</h1>
-        </div>
+    
+    // flex-1 -> get all the space of a flex container father 
+    return (
+        <form className={'fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100'}>
+            <input type={'text'} placeholder={"Enter a Message here..."} className="
+                flex-1 rounded border border-gray-300 focus:outline-none 
+                focus:ring-2 focus:ring-blue-600 focus:border-transparent 
+                px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed
+            " />
+            <button type={'submit'} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed">
+                Send
+            </button>
+        </form>
     )
 }
 
