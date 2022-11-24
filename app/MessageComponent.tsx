31b752b99b96f647f81d0d2b97fc4207a7b76475
guisantosalves <workguisantos@gmail.com>
2022-11-24 10:12:08 -0400

    making change in message list component

--- app/MessageComponent.tsx
+++ app/MessageComponent.tsx
@@ -10,9 +10,12 @@ type Props = {
     width: fit-content -> Use the space you can (available) but never less than your min-content and never more than your max-content 
 */
 const MessageComponent = ({ message }: Props) => {
+
+    const isUser = true;
+
     return (
-        <div className="flex w-fit">
-            <div>
+        <div className={`flex w-fit ${isUser && 'ml-auto'}`}>
+            <div className={`flex-shrink-0 ${isUser && 'order-2'}`}>
                 <Image
                     className="rounded-full mx-2"
                     src={message.profilePic}
@@ -23,14 +26,20 @@ const MessageComponent = ({ message }: Props) => {
             </div>
 
             <div>
-                <p>{message.username}</p>
-                
-                <div>
-                    <div>
-                       <p>{message.message}</p> 
+                <p className={`text-[0.65rem] px-[2px] pb-[2px] 
+                ${isUser ? 'text-blue-400 text-right' : 'text-red-400 text-left'}`}>
+                    {message.username}
+                </p>
+
+                <div className="flex items-end">
+                    <div className={`px-3 py-2 rounded-lg w-fit text-white
+                    ${isUser ? 'bg-blue-400 ml-auto order-2 text-right' : 'bg-red-400 text-left'}`}>
+                        <p>{message.message}</p>
                     </div>
 
-                    <p>{new Date(message.created_at).toLocaleString()}</p>
+                    <p className="text-[0.65rem] italic px-2 text-gray-400">
+                        {new Date(message.created_at).toLocaleString()}
+                    </p>
                 </div>
             </div>
         </div>
--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -9,9 +9,10 @@ const MessageList = () => {
     // getting from the end point
     const { data: messages, error, mutate } = useSWR('/api/getMessages', fetcher)
 
+    // we need to get message that the db has but doesn't show it yet
     console.log(messages)
     return (
-        <div>
+        <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
             {messages?.map((item, index) => (
                 <MessageComponent
                     key={index}
