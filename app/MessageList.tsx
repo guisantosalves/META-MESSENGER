4e9ffdc7f8d4bab3602cebceb06d06872ce18d5b
guisantosalves <workguisantos@gmail.com>
2022-11-24 12:37:09 -0400

    setting pusher and setting ssr with data on getMessages end point

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -3,17 +3,53 @@ import * as React from 'react'
 import useSWR from 'swr';
 import fetcher from '../utils/fetchMessages';
 import MessageComponent from './MessageComponent';
+import { Message } from '../typings';
+import { clientPusher } from '../pusher';
 
-const MessageList = () => {
+// subscribe channel -> channel has an event -> gets the event -> get the data of the event -> update in all application
+/*
+    como mutate funciona -> Em muitos casos, aplicar mutações locais aos dados é uma boa maneira de fazer 
+    alterações parecerem mais rápidas — não precisa esperar para a fonte de dados remota.
+    Com mutate, você pode atualizar seus dados localmente, enquanto revalidar e depois substituir por os dados mais recentes.
+*/
+
+type Props = {
+    initialMessages: Message[]
+}
+
+const MessageList = ({initialMessages}: Props) => {
 
     // getting from the end point
-    const { data: messages, error, mutate } = useSWR('/api/getMessages', fetcher)
+    const { data: messages, error, mutate } = useSWR<Message[]>('/api/getMessages', fetcher)
 
-    // we need to get message that the db has but doesn't show it yet
-    console.log(messages)
+    React.useEffect(() => {
+        const channel = clientPusher.subscribe('messages') // channel
+
+        // bind gets into the event
+        channel.bind('new-message', async (data: Message) => {
+
+            // if you sent the message, no need to update cache of swr
+            if (messages?.find((item) => item.id === data.id)) return;
+
+            if (!messages) {
+                mutate(fetcher)
+            } else {
+                mutate(fetcher, {
+                    optimisticData: [data, ...messages!],
+                    rollbackOnError: true,
+                })
+            }
+
+        })
+
+        return () => {
+            channel.unbind_all();
+            channel.unsubscribe();
+        }
+    }, [messages, mutate, clientPusher])
     return (
         <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
-            {messages?.map((item, index) => (
+            {(messages || inititalMessages).map((item, index) => (
                 <MessageComponent
                     key={index}
                     message={item}
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,29 @@ import * as React from 'react'
 // component
 import MessageList from './MessageList';
 import ChatInput from './ChatInput';
+import { Message } from '../typings';
 
-function HomePage() {
-    return(
+/*
+    chat input -> faço o push da menssagem no banco e faço o set dela
+    no store do swr pelo mutate -> fazendo isso, não vai precisar refazer 
+    validação nenhuma, simplesmente já renderiza
+
+    no caso de outra pessoa no chat:
+    messagelist -> ele valida o clientPusher, mutate e o messages,
+    ele faz a inserção dos dados do evento a cada evento que 
+    é lançado no pusher.
+*/
+async function HomePage() {
+    const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then((res) => {
+        return res.json()
+    })
+
+    const messages: Message[] = data.messages;
+
+    return (
         <main>
             {/* message list */}
-            <MessageList />
+            <MessageList initialMessages={messages}/>
 
             {/* chat input */}
             <ChatInput />
